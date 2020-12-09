ffd56e97c98f820b798653fe87374c639575d1e8
Tanveer Ahmed <37258978+Tan66@users.noreply.github.com>
2020-12-09 09:35:08 +0530

    updated app.py

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-import socket
+# import socket
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the socket to the port
-server = socket.gethostname()
-port = 8080
-server_address = (server, port)
-sock.bind(server_address)
-sock.listen(1)
+# # Create a TCP/IP socket
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# # Bind the socket to the port
+# server = socket.gethostname()
+# port = 8080
+# server_address = (server, port)
+# sock.bind(server_address)
+# sock.listen(1)
 
 app = Flask(__name__)
 
